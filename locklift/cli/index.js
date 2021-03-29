810e81bc2cfa2792f0303c5d9f37c8c3aa8be61d
Sergey P <pavlovdog.moscow@gmail.com>
2021-03-29 20:24:52 +0200

    Add version flag

--- locklift/cli/index.js
+++ locklift/cli/index.js
@@ -8,5 +8,6 @@ program.addCommand(require('./commands/build'));
 program.addCommand(require('./commands/test'));
 program.addCommand(require('./commands/run'));
 
+program.version(require('./../../package.json').version);
 
 program.parse(process.argv);
