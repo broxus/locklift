bcacb398d8b67e2cda4eb633a091caaa929ca84b
Maga <steam.30mb1@gmail.com>
2022-07-15 08:53:27 +0300

    Bumped compiler version

--- sample-project-typescript/locklift.config.ts
+++ sample-project-typescript/locklift.config.ts
@@ -14,7 +14,7 @@ const config: LockliftConfig = {
     // path: "/mnt/o/projects/broxus/TON-Solidity-Compiler/build/solc/solc",
 
     // Or specify version of compiler
-    version: "0.62.0",
+    version: "0.61.2",
 
     // Specify config for extarnal contracts as in exapmple
     // externalContracts: {
