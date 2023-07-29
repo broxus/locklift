239405ac9f988834fc7caa7f920cb6c4f7094960
cyace84 <cyace84@gmail.com>
2023-07-29 18:32:03 +0200

    add demos components & diagrams

---
+++ docs/src/components/demos/ComputeActionPhases.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="demo">
+    <button @click="increaseState(0)">Call Contract</button>
+    <button @click="increaseState(1)">Call with Gas Failure</button>
+    <button @click="increaseState(2)">Failure with 256 actions</button>
+    <pre class="event-data" v-if="eventData">{{ eventData }}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { Address, ProviderRpcClient } from 'everscale-inpage-provider';
+import { testContract, toNano } from './../../helpers';
+
+enum TypeAction {
+  Success = 0,
+  GasFailure = 1,
+  ActionPhaseFailure256 = 2,
+}
+
+const provider = new ProviderRpcClient();
+
+export default defineComponent({
+  name: 'IncreaseStateComponent',
+  setup() {
+    const eventData = ref();
+
+    return { eventData };
+  },
+  methods: {
+    async increaseState(type: TypeAction) {
+      await provider.ensureInitialized();
+      const { accountInteraction } = await provider.requestPermissions({
+        permissions: ['basic', 'accountInteraction'],
+      });
+
+      const exampleContract = new provider.Contract(
+        testContract.ABI,
+        new Address(testContract.address)
+      );
+      const senderAddress = accountInteraction?.address!;
+
+      const subscriber = new provider.Subscriber();
+      const contractEvents = exampleContract.events(subscriber);
+
+      const eventCallback = (event: any) => {
+        this.eventData = JSON.stringify(event, null, 2);
+        contractEvents.stopProducer();
+      };
+
+      contractEvents.on(eventCallback);
+      console.log('type', type);
+      if (type === TypeAction.Success) {
+        const payload = {
+          abi: JSON.stringify(testContract.ABI),
+          method: 'increaseState',
+          params: {
+            count: 253,
+          },
+        };
+        await provider.sendMessage({
+          sender: senderAddress,
+          recipient: new Address(testContract.address),
+          amount: toNano(1),
+          bounce: true,
+          payload: payload,
+        });
+      } else if (type === TypeAction.GasFailure) {
+        console.log('gas failure');
+        const payload = {
+          abi: JSON.stringify(testContract.ABI),
+          method: 'increaseState',
+          params: {
+            count: 253,
+          },
+        };
+        const { transaction: tx } = await provider.sendMessage({
+          sender: senderAddress,
+          recipient: new Address(testContract.address),
+          amount: toNano(0.001),
+          bounce: true,
+          payload: payload,
+        });
+
+        const traceStream = subscriber.trace(tx);
+
+        traceStream.on(data => {
+          this.eventData = JSON.stringify(data, null, 2);
+          traceStream.stopProducer();
+        });
+      } else if (type === TypeAction.ActionPhaseFailure256) {
+        const payload = {
+          abi: JSON.stringify(testContract.ABI),
+          method: 'increaseState',
+          params: {
+            count: 254,
+          },
+        };
+        const { transaction: tx } = await provider.sendMessage({
+          sender: senderAddress,
+          recipient: new Address(testContract.address),
+          amount: toNano(1),
+          bounce: true,
+          payload: payload,
+        });
+
+        const traceStream = subscriber.trace(tx);
+
+        traceStream.on(data => {
+          this.eventData = JSON.stringify(data, null, 2);
+          traceStream.stopProducer();
+        });
+      }
+    },
+  },
+});
+</script>
---
+++ docs/src/components/demos/TransactionFinalization.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="demo">
+    <button @click="setOtherState()">Call Contract A</button>
+    <div v-if="prevState1">
+      <pre>prevState_A: {{ prevState1 }}</pre>
+      <pre>prevState_B: {{ prevState2 }}</pre>
+    </div>
+    <div v-if="transaction">
+      <pre>newState_A: {{ newState1 }}</pre>
+      <pre>newState_B: {{ newState2 }}</pre>
+      <pre>Transaction: {{ transaction }}</pre>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue';
+import { Address, ProviderRpcClient } from 'everscale-inpage-provider';
+import { testContract, toNano } from './../../helpers';
+
+const provider = new ProviderRpcClient();
+
+export default defineComponent({
+  name: 'IncreaseStateComponent',
+  setup() {
+    const prevState1 = ref('');
+    const prevState2 = ref('');
+    const newState1 = ref('');
+    const newState2 = ref('');
+    const transaction = ref('');
+
+    async function fetchStates(state_1: Ref<string>, state_2: Ref<string>) {
+      const exampleContract_1 = new provider.Contract(
+        testContract.ABI,
+        new Address(testContract.address)
+      );
+      const exampleContract_2 = new provider.Contract(
+        testContract.ABI,
+        new Address(testContract.dublicateAddress)
+      );
+
+      const { _state: newState_1 } = await exampleContract_1.methods.getDetails().call();
+      const { _state: newState_2 } = await exampleContract_2.methods.getDetails().call();
+
+      state_1.value = newState_1;
+      state_2.value = newState_2;
+    }
+
+    async function setOtherState() {
+      try {
+        await provider.ensureInitialized();
+        const { accountInteraction } = await provider.requestPermissions({
+          permissions: ['basic', 'accountInteraction'],
+        });
+
+        const exampleContract_1 = new provider.Contract(
+          testContract.ABI,
+          new Address(testContract.address)
+        );
+
+        const senderAddress = accountInteraction?.address!;
+
+        await fetchStates(prevState1, prevState2);
+
+        const { _state: prevState_1 } = await exampleContract_1.methods.getDetails().call();
+
+        const payload = {
+          abi: JSON.stringify(testContract.ABI),
+          method: 'setOtherState',
+          params: {
+            other: new Address(testContract.dublicateAddress),
+            _state: Number(prevState_1) + 1,
+            count: 256,
+          },
+        };
+        const { transaction: tx } = await provider.sendMessage({
+          sender: senderAddress,
+          recipient: new Address(testContract.address),
+          amount: toNano(0.3),
+          bounce: true,
+          payload: payload,
+        });
+
+        const subscriber = new provider.Subscriber();
+        const traceStream = subscriber.trace(tx);
+
+        traceStream.on(async data => {
+          if (data.aborted) {
+            traceStream.stopProducer();
+            await fetchStates(newState1, newState2);
+            transaction.value = JSON.stringify(data, null, 2);
+          }
+        });
+      } catch (err: any) {
+        transaction.value = err.message || 'Unknown Error';
+      }
+    }
+
+    return {
+      prevState1,
+      prevState2,
+      newState1,
+      newState2,
+      transaction,
+      setOtherState,
+    };
+  },
+});
+</script>
